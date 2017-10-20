ce35e54c61eccbfe11d016e7c2c63f892b7d6bf3
Sherida van den Bent <sheridavandenbent@gmail.com>
2017-10-20 15:24:01 +0200

    finish runall script for realzzz

--- runall.sh
+++ runall.sh
@@ -29,14 +29,11 @@ do
                                 do
                                     for prob_pick_best in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
                                     do
-                                        echo "\n"
-                                        java -DPARENTSELECT_STYLE="$PARENTSELECT_STYLE" -DREPRODUCE_STYLE="$REPRODUCE_STYLE" -DSURVIVAL_STYLE="$SURVIVAL_STYLE" -Dq="$q" -Dpop_size="$pop_size" -Doff_size="$off_size" -Dstd_dev_th="$std_dev_th" -Dtournament_size="$tournament_size" -Dprob_pick_best="$prob_pick_best" -jar testrun.jar -submission=player15 -evaluation=SchaffersEvaluation -seed=1
-
                                         for i in `seq 4`
                                         do
                                             if (( (count % proccount) == 0)); then
-                                                score=`java -DPARENTSELECT_STYLE="$PARENTSELECT_STYLE" -DREPRODUCE_STYLE="$REPRODUCE_STYLE" -DSURVIVAL_STYLE="$SURVIVAL_STYLE" -Dq="$q" -Dpop_size="$pop_size" -Dstd_dev_th="$std_dev_th" -Dtournament_size="$tournament_size" -Dprob_pick_best="$prob_pick_best" -jar testrun.jar -submission=player15 -evaluation=BentCigarFunction -seed=1 | grep Score | cut -d' ' -f 2`
-                                                echo "$ef,$PARENTSELECT_STYLE,$REPRODUCE_STYLE,$SURVIVAL_STYLE,$pop_size,$std_dev_th,$tournament_size,$prob_pick_best,$score" >> $ef-statistics.txt
+                                                score=`java -DPARENTSELECT_STYLE="$PARENTSELECT_STYLE" -DREPRODUCE_STYLE="$REPRODUCE_STYLE" -DSURVIVAL_STYLE="$SURVIVAL_STYLE" -Dq="$q" -Dpop_size="$pop_size" -Doff_size="$off_size" -Dstd_dev_th="$std_dev_th" -Dtournament_size="$tournament_size" -Dprob_pick_best="$prob_pick_best" -jar testrun.jar -submission=player15 -evaluation=BentCigarFunction -seed=1 | grep Score | cut -d' ' -f 2`
+                                                echo "$ef,$PARENTSELECT_STYLE,$REPRODUCE_STYLE,$SURVIVAL_STYLE,$pop_size,$std_dev_th,$tournament_size,$prob_pick_best,$off_size,$score" >> $ef-statistics.txt
                                             fi
                                             count=$((count+1))
                                         done
