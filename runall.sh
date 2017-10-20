375e55b3bf85502eb6d13ca98d0c3fd105b8c258
Sherida van den Bent <sheridavandenbent@gmail.com>
2017-10-20 16:05:44 +0200

    hopefully final adjustment to runall.sh

--- runall.sh
+++ runall.sh
@@ -17,19 +17,19 @@ do
         do
             for SURVIVAL_STYLE in 1 2
             do
-                for q in 5 10 20 50 100 200
+                for q in 10
                 do
-                    for off_size in 10 50 100 250 500 1000
+                    for off_size in 50 100 250
                     do
-                        for pop_size in 10 50 100 250 500 1000
+                        for pop_size in 50 100 250
                         do
-                            for std_dev_th in 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2
+                            for std_dev_th in 0.2 0.6 1.0 1.4 1.8 2
                             do
-                                for tournament_size in 5 10 20 40 80
+                                for tournament_size in 10 20 40
                                 do
-                                    for prob_pick_best in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+                                    for prob_pick_best in 0.6 0.8 1.0
                                     do
-                                        for i in `seq 4`
+                                        for i in `seq 24`
                                         do
                                             if (( (count % proccount) == 0)); then
                                                 score=`java -DPARENTSELECT_STYLE="$PARENTSELECT_STYLE" -DREPRODUCE_STYLE="$REPRODUCE_STYLE" -DSURVIVAL_STYLE="$SURVIVAL_STYLE" -Dq="$q" -Dpop_size="$pop_size" -Doff_size="$off_size" -Dstd_dev_th="$std_dev_th" -Dtournament_size="$tournament_size" -Dprob_pick_best="$prob_pick_best" -jar testrun.jar -submission=player15 -evaluation=BentCigarFunction -seed=1 | grep Score | cut -d' ' -f 2`
